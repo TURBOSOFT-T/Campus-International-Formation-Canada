97816c5b5a7efa632c7048b7cce6b9b298a25555
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2025-10-16 02:02:08 +0200

    format code

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -45,7 +45,7 @@ $categories = DB::table('categories')->get();
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
  --}}
-    <script src="/Script.js"></script>
+   {{--  <script src="/Script.js"></script> --}}
     @yield('SEO')
     @yield('services')
     @yield('blogs')
